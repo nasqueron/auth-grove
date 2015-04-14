969aa94a2325cb6d431c24dc03ec94912f6cdd34
Sébastien Santoro <dereckson@espace-win.org>
2015-04-14 12:22:36 +0000

    Allowed to set database prefix in env configuration
    New environment variable: DB_PREFIX
    Defaut value: (empty string)
    
    Note: when define a prefix, user  should include the trailing _.

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
 		'sqlite' => [
 			'driver'   => 'sqlite',
 			'database' => storage_path().'/database.sqlite',
-			'prefix'   => '',
+			'prefix'   => env('DB_PREFIX', ''),
 		],
 
 		'mysql' => [
@@ -71,7 +71,7 @@ return [
 			'username' => env('DB_USERNAME', 'forge'),
 			'password' => env('DB_PASSWORD', ''),
 			'charset'  => 'utf8',
-			'prefix'   => '',
+			'prefix'   => env('DB_PREFIX', ''),
 			'schema'   => 'public',
 		],
 
@@ -81,7 +81,7 @@ return [
 			'database' => env('DB_DATABASE', 'forge'),
 			'username' => env('DB_USERNAME', 'forge'),
 			'password' => env('DB_PASSWORD', ''),
-			'prefix'   => '',
+			'prefix'   => env('DB_PREFIX', ''),
 		],
 
 	],
