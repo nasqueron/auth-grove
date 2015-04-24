2117ae116e06ea1ff76aec3013b8b68a368918bf
Sébastien Santoro <dereckson@espace-win.org>
2015-04-24 09:31:12 +0000

    [L10n] English messages for app title and login messages

---
+++ resources/lang/en/login.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Login
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the fields names, error messages and
+	| other strings related to the authentication itself.
+	|
+	*/
+
+	//Login form
+	"username" => "Username",
+	"password" => "Password",
+	"loginButton" => "Sign in",
+	"remember" => "Remember me",
+	"passwordRecovery" => "Password recovery options",
+	"blankAvatarAlt" => "A door, as a blank avatar",
+
+	//Error messages
+	"failedLogin" => "These credentials do not match our records.",
+];
