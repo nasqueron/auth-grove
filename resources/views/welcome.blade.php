06ae32f18bc8f84c31651f147648bf19bf66f7e9
Sébastien Santoro <dereckson@espace-win.org>
2015-05-09 10:17:06 +0000

    Errors handling in login screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,14 @@
                 <span id="reauth-username" class="reauth-username"></span>
                 <input type="text" name="username" id="inputUsername" class="form-control" value="{{ old('username') }}" placeholder="@lang('login.username')" required autofocus>
                 <input type="password" name="password" id="inputPassword" class="form-control" placeholder="@lang('login.password')" required>
-            </div>
+            </div>@if (count($errors) > 0)
+            <p class="errors">
+@foreach ($errors->all() as $error)
+                {{ $error }}<br/>
+@endforeach
+            <a href="{{ url('/auth/recover') }}" class="forgot-password">@lang('login.passwordRecovery')</a>
+            </p>
+            @endif
             <div id="remember" class="checkbox">
                 <label>
                     <input type="checkbox" name="remember"> @lang('login.remember')
@@ -26,7 +33,9 @@
             <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">@lang('login.loginButton')</button>
         </form>
         <!-- /form -->
+@if (count($errors) == 0)
         <a href="{{ url('/auth/recover') }}" class="forgot-password">@lang('login.passwordRecovery')</a>
+@endif
     </div>
     <!-- /card-container -->
 
