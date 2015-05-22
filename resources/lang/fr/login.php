9f9932a57f9c55c2c44a4dcd0fdc6ae7e3ac4308
Michaël Colignon <ledesillusionniste@gmail.com>
2015-05-22 14:33:00 +0000

    French translation
    Translated login information.
    
    Differential Revision: http://devcentral.nasqueron.org/D13

---
+++ resources/lang/fr/login.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * French translation by:
+ *   - Michaël Colignon (philectro)
+ */
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Login
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the fields names, error messages and
+	| other strings related to the authentication itself.
+	|
+	*/
+
+	// Login form
+	"username" => "Nom d'utilisateur",
+	"password" => "Mot de passe",
+	"loginButton" => "Se connecter",
+	"remember" => "Rester connecté",
+	"passwordRecovery" => "Récupération de mot de passe",
+	"blankAvatarAlt" => "Une porte, comme avatar par défaut.",
+
+	// Error messages
+	"failedLogin" => "Le login ou mot de passe est incorrect.",
+];
