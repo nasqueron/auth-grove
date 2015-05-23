139e41073ee65065039bf20ed8e02b0fb9d23e0f
Sébastien Santoro <dereckson@espace-win.org>
2015-05-23 09:29:16 +0000

    Beautifying HTML code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,20 +5,21 @@
         <form class="form-signin" role="form" method="POST" action="{{ url('/auth/login') }}">
             <div id="identity">
                 <span id="reauth-username" class="reauth-username"></span>
-                <input type="text" name="username" id="inputUsername" class="form-control" value="{{ old('username') }}" placeholder="@lang('login.username')" required autofocus>
-                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="@lang('login.password')" required>
+                <input type="text" name="username" id="inputUsername" class="form-control"
+                       value="{{ old('username') }}" placeholder="@lang('login.username')" required autofocus />
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="@lang('login.password')" required />
             </div>@if (count($errors) > 0)
+
             <p class="errors">
 @foreach ($errors->all() as $error)
-                {{ $error }}<br/>
+                {{ $error }}<br />
 @endforeach
-            <a href="{{ url('/auth/recover') }}" class="forgot-password">@lang('login.passwordRecovery')</a>
+                <a href="{{ url('/auth/recover') }}" class="forgot-password">@lang('login.passwordRecovery')</a>
             </p>
             @endif
+
             <div id="remember" class="checkbox">
-                <label>
-                    <input type="checkbox" name="remember"> @lang('login.remember')
-                </label>
+                <label><input type="checkbox" name="remember">@lang('login.remember')</label>
             </div>
             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
             <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">@lang('login.loginButton')</button>
--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -1,16 +1,18 @@
+<!DOCTYPE html>
 <html>
 
 <head>
     <title>@lang('app.title')</title>
-    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" rel='stylesheet' type='text/css'>
-    <link href="/css/login.css" rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+    <link href="/css/login.css" rel='stylesheet' type='text/css' />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body class="sviatiHory">
     <div class="card card-container">
         <h1 class="title">@lang('app.title')</h1>
-        @yield('card-content')
+@yield('card-content')
     </div>
     <!-- /card-container -->
 
