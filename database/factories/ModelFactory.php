0de31f3759ed6afca1db421aa5a482dcf936e0b6
Sébastien Santoro <dereckson@espace-win.org>
2015-06-02 17:15:40 +0000

    Fixed users table definition
    * Fixed username field name
    * Enabled soft delete

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,7 +13,7 @@
 
 $factory->define(App\User::class, function ($faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->username,
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
