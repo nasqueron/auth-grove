b7d136c230387b80bebc377c312ba4218cd8b731
Sébastien Santoro <dereckson@espace-win.org>
2015-06-09 11:51:29 +0000

    Fixed whitespace issues

--- MakeEnv.php
+++ MakeEnv.php
@@ -4,36 +4,36 @@
  * Generate an .env file from an .env.example file
  * in a Cloud9 environment, for Laravel framework.
  */
- 
+
 class EnvironmentFile {
     ///
     /// Default values
     ///
-    
+
     const DEFAULT_SOURCE = '.env.example';
     const DEFAULT_TARGET = '.env';
-    
+
     ///
     /// Properties
     ///
-    
+
     public $source;
     public $target;
     private $config;
-    
+
     ///
     /// Constructors
     ///
-    
+
     static public function load () {
         return new static(static::DEFAULT_SOURCE, static::DEFAULT_TARGET);
     }
-    
+
     public function __construct ($source, $target) {
         $this->source = $source;
         $this->target = $target;
     }
-    
+
     public function readDefaultConfiguration () {
         $lines = file($this->source);
         foreach ($lines as $line) {
@@ -42,17 +42,17 @@ class EnvironmentFile {
                 //Blank lines and comment are ignored
                 continue;
             }
-            
+
             $var = explode('=', $line, 2);
             $this->config[$var[0]] = $var[1];
         }
         return $this;
     }
-    
+
     private function getConfigurationLine ($key, $value) {
         return $key . '=' . $value;
     }
-    
+
     public function getConfiguration () {
         $lines = [];
         foreach ($this->config as $key => $value) {
@@ -61,17 +61,17 @@ class EnvironmentFile {
         }
         return implode("\n", $lines);
     }
-    
+
     public function save () {
         $configToWrite = $this->getConfiguration();
         file_put_contents($this->target, $configToWrite);
     }
-    
+
     public static function getRandomString ($len = 32) {
         $bytes = openssl_random_pseudo_bytes(++$len / 2);
         return substr(bin2hex($bytes), 0, $len);
     }
-    
+
     public function amendVariable ($key, $value) {
         switch ($key) {
             case APP_KEY:
@@ -79,20 +79,20 @@ class EnvironmentFile {
                     return static::getRandomString();
                 }
                 break;
-                
+
             case DB_HOST:
                 return getenv("IP");
-                
+
             case DB_DATABASE:
                 return 'c9';
-                
+
             case DB_USERNAME:
                 return getenv("C9_USER");
-                
+
             case DB_PASSWORD:
                 return '';
         }
-        
+
         return $value;
     }
 }
@@ -102,8 +102,6 @@ if (!file_exists('.env.example')) {
     die("First, create an .env.example file with the default settings.\n");
 }
 
-echo
-EnvironmentFile::load()
+echo EnvironmentFile::load()
     ->readDefaultConfiguration()
     ->getConfiguration();
-;
