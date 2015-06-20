b54f754466eec8929c3f99d7c5b84ec56bda3ee7
Sébastien Santoro <dereckson@espace-win.org>
2015-06-20 07:43:20 +0000

    [L10n] Localized account control panel blade template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,7 +24,7 @@
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
+					<span class="sr-only">@lang('panel.toggle-navigation')</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
@@ -34,7 +34,7 @@
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
+					<li><a href="{{ url('/') }}">@lang('panel.home')</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
@@ -45,7 +45,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+								<li><a href="{{ url('/auth/logout') }}">@lang('panel.logout')</a></li>
 							</ul>
 						</li>
 					@endif
