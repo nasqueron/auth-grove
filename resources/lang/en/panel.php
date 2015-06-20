a40e8316e9022b2708643604b7049728533f7eb6
Sébastien Santoro <dereckson@espace-win.org>
2015-06-20 07:39:11 +0000

    [L10n] English messages for the account control panel

---
+++ resources/lang/en/panel.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| E-mail content
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used in mails sent by Auth Grove.
+	|
+	*/
+
+	///
+	/// Navigation
+	///
+	
+	'home' => 'Home',
+	'logout' => 'Logout',
+	'toggle-navigation' => 'Toggle Navigation',
+	
+	///
+	/// Home - Status
+	///
+	
+	'status' => 'Status',
+	'loggedin' => "You're logged in.",
+	
+	///
+	/// Home - Account
+	///
+	
+	'account' => 'Account information',
+	'user-attributes' => [
+	    "username" => "Username",
+	    "fullname" => "Full name",
+	    "email" => "E-mail",
+	    "created_at" => "Account created",
+	    "updated_at" => "Last updated",
+    ],
+	
+];
