4cf98386e80c24dc8195960904c2f821cc511702
Sébastien Santoro <dereckson@espace-win.org>
2015-06-20 07:33:56 +0000

    [L10n] Localized home blade template
    New keys in panel:
    * loggedin
    * status

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">@lang('panel.status')</div>
 
 				<div class="panel-body">
-					You are logged in!
+					@lang('panel.loggedin')
 				</div>
 			</div>
 		</div>
