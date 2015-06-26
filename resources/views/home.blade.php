302714c9b96bc4d1d742d85145dbba2398e61f1a
Sébastien Santoro <dereckson@espace-win.org>
2015-06-26 11:32:13 +0000

    Account information on login dashboard
    Show core properties like username, full name, e-mail,
    account creation and last updated dates.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,26 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">@lang('panel.account')</div>
+
+				<div class="panel-body">
+					<table class="table table-striped">
+						<tbody>
+@foreach ($user as $key => $attribute)
+							<tr>
+								<th>@lang("panel.user-attributes.$key")</th>
+								<td>{{ $attribute }}</td>
+							</tr>
+@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
