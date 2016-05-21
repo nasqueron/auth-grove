ce387050c995cc36f9a124cc47902bf6d96dbf2e
Sébastien Santoro <dereckson@espace-win.org>
2016-05-21 20:32:04 +0000

    Update database schema for users table
    Summary:
    Commits f60cd7aa and b273524a adjusted the users table schema
    to include a full name field. Schema is adjusted accordingly.
    
    Fixes T836.
    
    Test Plan:
      - Perform a fresh installation
      - `php artisan account:dump`
    
    Reviewers: dereckson
    
    Maniphest Tasks: T836
    
    Differential Revision: http://devcentral.nasqueron.org/D378

--- database/migrations/2015_05_06_081119_create_users_table.php
+++ database/migrations/2015_05_06_081119_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('username');
+			$table->string('fullname');
 			$table->string('email')->unique();
 			$table->string('password', 60);
 			$table->rememberToken();
