43423833776e613e5853cafbc2e39912ee73665f
Sébastien Santoro <dereckson@espace-win.org>
2016-06-01 14:40:56 +0000

    Integration tests for auth views
    Summary:
    These basic integration tests emulate GET requests to each auth
    view and checks if the expected form is available.
    
    Test Plan: `phpunit`
    
    Reviewers: dereckson
    
    Differential Revision: http://devcentral.nasqueron.org/D383

---
+++ tests/integration/AuthViewsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Test authentication views with GET request and form title checks.
+ * 
+ * A test failure means an auth view is broken.
+ */
+class AuthViewsTest extends TestCase {
+
+    /**
+     * Tests the sign in view
+     */
+    public function testSignInView() {
+        $this->visit('/')
+             ->see('form-signin');
+    }
+    
+    /**
+     * Tests the register account view
+     */
+    public function testRegisterAccountView() {
+        $this->visit('/auth/register')
+             ->see('form-register');
+    }
+    
+    /**
+     * Tests the recover access view
+     */
+    public function testRecoverAccessView() {
+        $this->visit('/auth/recover')
+             ->see('form-recover');
+    }
+    
+    /**
+     * Tests the reset password view
+     */
+    public function testResetPasswordView() {
+        $this->visit('/auth/reset/foo')
+             ->see('form-reset');
+    }
+    
+}
