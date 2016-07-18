b2d077f8b9715b73fdf085ea0d16116454d269ba
Sébastien Santoro <dereckson@espace-win.org>
2016-07-18 23:19:55 +0000

    Improve AppServiceProvider class
    Summary: Remove legacy code comment. Style.
    
    Test Plan: no op
    
    Reviewers: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D520

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,13 +26,11 @@ class AppServiceProvider extends ServiceProvider {
 	 * Register any application services.
 	 *
 	 * This service provider is a great spot to register your various container
-	 * bindings with the application. As you can see, we are registering our
-	 * "Registrar" implementation here. You can add your own bindings too!
+	 * bindings with the application.
 	 *
 	 * @return void
 	 */
-	public function register()
-	{
+	public function register() {
 	}
 
 }
